b06d58d9176c8c05b8197c8e8fd946d603e17474
Fayzullo <yusupovfayzullo100@gmail.com>
2022-11-19 22:32:56 +0500

    O'zgardi

--- src/exercise/MatrixExample.java
+++ src/exercise/MatrixExample.java
@@ -2,26 +2,26 @@ package exercise;
 
 public class MatrixExample {
 
-        public static void main(String args[]){
+    public static void main(String args[]) {
 
-            int a[][]={{1,1,1,1},{2,2,2,2},{3,3,3,3}};
-            int b[][]={{1,1,1,1,1},{2,2,2,2,2},{3,3,3,3,3},{4,4,4,4,4}};
-
-
-            int c[][]=new int[3][5];
+        int a[][] = {{1, 1, 1, 1}, {2, 2, 2, 2}, {3, 3, 3, 3}};
+        int b[][] = {{1, 1, 1, 1, 1}, {2, 2, 2, 2, 2}, {3, 3, 3, 3, 3}, {4, 4, 4, 4, 4}};
 
 
-            for(int i=0;i<3;i++){
-                for(int j=0;j<5;j++){
-                    c[i][j]=0;
-                    for(int k=0;k<3;k++)
-                    {
-                        c[i][j]+=a[i][k]*b[k][j];
-                    }
-                    System.out.print(c[i][j]+" ");
+        int c[][] = new int[3][5];
+
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                c[i][j] = 0;
+                for (int k = 0; k < 3; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
                 }
-                System.out.println();
+                System.out.print(c[i][j] + " ");
             }
+            System.out.println();
         }
+        System.out.println("Done");
+    }
 }
 
